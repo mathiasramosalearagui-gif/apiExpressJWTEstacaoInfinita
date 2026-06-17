77bcca7d558c6bf1248f4f36ab3e4d2d004964c7
guilherme <mathiasramosalearagui@gmail.com>
2026-06-17 16:41:23 -0300

    AuthMiddleware and .env update

---
+++ Back/middlewares/authMiddleares.js
@@ -0,0 +1,39 @@
+import User from "../models/User";
+import jwt from "jsonwebtoken";
+
+const authenticate = async (req, res, next) => {
+    try {
+        const authorization = req.header.authorization
+        if (!authorization) {
+            throw res.ststus(401).json({ message: "The token is required."})
+        }
+
+        const parts = authorization.split(" ");
+        if (parts !== 2) {
+            throw res.status(401).json({ message: "The token is incorreted."})
+        }
+
+        const [scheme, token] = parts;
+        if (scheme !== "Bearer") {
+            throw res.status(401).json({ mesage: "The token is not correct."})
+        }
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+        const user = await User.findById(decoded._id);
+        if (!user) {
+            throw res.status(401).json({ message: "User dont find." })
+        }
+
+        if (!user.active) {
+            throw res.status(401).json({ message: "This user is unactive. Please into contact with the team."})
+        }
+
+        req.user = user;
+
+        next()
+
+    } catch (error) {
+        next(error)
+    }
+}
