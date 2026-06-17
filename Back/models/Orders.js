6f19ca332371d6d2fc02068cf7bba8881a343e3e
Gabriel <gr8043736@gmail.com>
2026-06-17 16:22:31 -0300

    modelOrders

---
+++ Back/models/Orders.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+
+const OrdersSchema = new mongoose.Schema(
+    {
+        status: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        paymentMethod: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        idUser: {
+            type: ObjectId,
+            required: true,
+            trim: true
+        },
+        products: {
+            type: Array,
+            required: true,
+            trim: true
+        },
+        totalQuantity: {
+            type: Number,
+            required: true,
+            trim: true
+        },
+        totalprice: {
+            type: Number,
+            required: true,
+            trim: true
+        }
+    
+
+ },
+    {
+        collection: "orders",
+        timestamps: true
+    }
+)
+
+export default mongoose.model("Orders", OrdersSchema);
