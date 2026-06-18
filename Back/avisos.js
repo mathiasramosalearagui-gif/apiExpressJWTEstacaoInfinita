7f9c62f2d8de640951f78c12e5d54c1bfc59672d
guilherme <mathiasramosalearagui@gmail.com>
2026-06-18 14:24:22 -0300

    criei um mural de avisos e dicas

---
+++ Back/avisos.js
@@ -0,0 +1,17 @@
+//git branch  === lista as branch
+//git branch algo === criar a branch "algo"
+//git switch algo === entra na branch "algo"
+//
+//feature/criar algo
+//fix/corrige algo
+//
+//Boa pratica: 
+//1- sempre nomeie a branch conforme o q vc tá fazendo
+//EX: git branch fix/correcao
+//
+//
+//
+//
+//
+//
+//
