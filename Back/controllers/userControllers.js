4d08e676431af248257c6f25075b6bfc3eb39b25
Gabriel <gr8043736@gmail.com>
2026-06-23 16:08:35 -0300

    rotaupdate

--- Back/controllers/userControllers.js
+++ Back/controllers/userControllers.js
@@ -11,6 +11,21 @@ const getMe = async (req, res, next) => {
     }
 }
 
+const updateMe = async (req, res, next) => {
+    try {
+        const user = await userService.updateMe(req.user._id, req.body)
+
+        res.status(200).json({
+            user,
+            message: "Profile updated successfully",
+
+        })
+    } catch (error) {
+        next(error)
+    }
+}
+
 export default {
-    getMe
+    getMe,
+    updateMe
 }
