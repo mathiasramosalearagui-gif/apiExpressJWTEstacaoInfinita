2ce5eb19e1a52015d99d8bc82c59e7d546030f17
guilherme <mathiasramosalearagui@gmail.com>
2026-06-30 14:32:42 -0300

    Criei o adminMiddleware

--- Back/routes/adminRoutes.js
+++ Back/routes/adminRoutes.js
@@ -1,17 +1,18 @@
 import adminControllers from "../controllers/adminControllers.js";
 import express from "express";
 import authMiddlewares from "../middlewares/authMiddleares.js";
+import adminMiddlewares from "../middlewares/adminMiddlewares.js";
 
 const router = express.Router()
 
-router.get("/users", authMiddlewares, adminControllers.listAllUsers)
-router.get("/relatory", authMiddlewares, adminControllers.relatory)
-router.get("/products", authMiddlewares, adminControllers.listProducts)
-router.post("/products/new", authMiddlewares, adminControllers.createProduct)
-router.put("/products/:product", authMiddlewares, adminControllers.updateProduct)
-router.patch("/products/:product/desactive", authMiddlewares, adminControllers.desactiveProduct)
-router.patch("/products/:product/active", authMiddlewares, adminControllers.activeProduct)
-router.delete("/products/:product/deleted", authMiddlewares, adminControllers.deleteProduct)
-router.get("/sales", authMiddlewares, adminControllers.getSales)
+router.get("/users", authMiddlewares, adminMiddlewares, adminControllers.listAllUsers)
+router.get("/relatory", authMiddlewares, adminMiddlewares, adminControllers.relatory)
+router.get("/products", authMiddlewares, adminMiddlewares, adminControllers.listProducts)
+router.post("/products/new", authMiddlewares, adminMiddlewares, adminControllers.createProduct)
+router.put("/products/:product", authMiddlewares, adminMiddlewares, adminControllers.updateProduct)
+router.patch("/products/:product/desactive", authMiddlewares, adminMiddlewares, adminControllers.desactiveProduct)
+router.patch("/products/:product/active", authMiddlewares, adminMiddlewares, adminControllers.activeProduct)
+router.delete("/products/:product/deleted", authMiddlewares, adminMiddlewares, adminControllers.deleteProduct)
+router.get("/sales", authMiddlewares, adminMiddlewares, adminControllers.getSales)
 
 export default router
